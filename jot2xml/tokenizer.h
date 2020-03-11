aaa5f382d75c6f11a757fdfa4feb5637127e9c37
t-d-k <github.tdk@squte.com>
2020-03-11 12:23:56 +0000

    update for VS code

--- jot2xml/tokenizer.h
+++ jot2xml/tokenizer.h
@@ -66,3 +66,10 @@ public:
 };
 
 wstring unescapeText(const wstring line);
+
+// defined by default in gcc
+/* Old compatibility names for C types.  */
+typedef unsigned long int ulong;
+typedef unsigned short int ushort;
+typedef unsigned int uint;
+
